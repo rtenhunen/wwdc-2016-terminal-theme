4fe463afda3c5734a7345b2d6036601daefa4f20
Rainer Tenhunen <tenhunen.r@apple.com>
2016-09-28 15:06:53 -0700

    better var assignment for test_OS_version ()

--- theme_test_case.sh
+++ theme_test_case.sh
@@ -17,16 +17,17 @@ LIGHT_GREEN=$(tput setaf 10)
 LILAC=$(tput setaf 13) # "Bright Magenta"
 
 
-function test_OS_version () {
-	MAJOR=$(sw_vers -productVersion | awk -F "." '{print $1}')
-	MINOR=$(sw_vers -productVersion | awk -F "." '{print $2}')
+function test_OS_version () { #  (Nil) -> String
+	# Does system check to make sure this is used on 10.12 or later
+	# SF Mono shipped bundled in Terminal.app on 10.12. Using the theme in earlier versions makes the system pick random fonts and letter spacing
 
+	read MAJOR MINOR <<< $(sw_vers -productVersion | awk -F "." '{print $1" "$2}')
 	if [[ $MAJOR -lt 10 || $MINOR -lt 12 ]]; then
 		printf "${YELLOW}WARNING:\t${GREEN}W${MAGENTA}W${YELLOW}D${RED}C${CYAN}16${NORMAL} theme uses ${MAGENTA}\"SF Mono\"${NORMAL} font which is bundled with ${CYAN}mac${GREEN}OS ${YELLOW}10${NORMAL}.${RED}12 ${LILAC}Sierra${NORMAL}.\n\t\tYou should either update your OS, or make sure you change the terminal font.\n\n" 
 	fi
 }
 
-function print_wwdc_intro () {
+function print_wwdc_intro () { # (Nil) -> String
 	printf "${GREEN}W${MAGENTA}W${YELLOW}D${RED}C${CYAN}16${NORMAL} theme - now in terminal (idea for the theme inspired by ${CYAN}developer${NORMAL}.${GREEN}apple${NORMAL}.${YELLOW}com${NORMAL}/${RED}wwdc${NORMAL})\n\n"
 	printf "${CYAN}print" # teal
 	printf "${NORMAL}(" # white
@@ -35,15 +36,15 @@ function print_wwdc_intro () {
 	printf "${LILAC}Hello love at first swipe.\n" # lilac
 	printf "${YELLOW}Hello other side of the road.\n" # yellow
 	printf "${RED}Hello yogi on my wrist.\n" # red
-	printf "${MAGENTA}Hello driver, fast as you can.\n" # 
-	printf "${CYAN}Hello workout in my living room.\n"
-	printf "${RED}Hello every pitch, every highlight.\n"
-	printf "${GREEN}Hello self-combusting selfies.\n"
-	printf "${MAGENTA}Hello double tap heart.\n"
+	printf "${MAGENTA}Hello driver, fast as you can.\n" # magenta
+	printf "${CYAN}Hello workout in my living room.\n" # cyan
+	printf "${RED}Hello every pitch, every highlight.\n" # red
+	printf "${GREEN}Hello self-combusting selfies.\n" # green
+	printf "${MAGENTA}Hello double tap heart.\n" # mangenta
 	printf "${YELLOW}Hello rain in five minutes.\n" # Yellow
-	printf "${GREEN}Hello 6 seconds of fame.\n"
-	printf "${LILAC}Hello big idea.\n\n\n"
-	printf "${LIGHT_GREEN}/* San Francisco, June 13-17 */\n\n"
+	printf "${GREEN}Hello 6 seconds of fame.\n" # green
+	printf "${LILAC}Hello big idea.\n\n\n" # lilac
+	printf "${LIGHT_GREEN}/* San Francisco, June 13-17 */\n\n" # light-green
 	printf "${NORMAL}learnMore()\n\n"
 }
 
